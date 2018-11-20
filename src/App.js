4c20b75681136e3837269189169f0d81119b3499
Davos DeHoyos <therealdavos@gmail.com>
2018-11-20 16:16:58 -0600

    First Commit for this project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Form, Field } from 'react-final-form'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
-export default App;
+const sleep = ms => new Promise(resolve => setTimeout(resolve.ms))
+const showResults = async values => {
+  await sleep(500) //simulate Server latency
+}
+const required =value => (value ? undefined : 'Required' )
+const App = () => (
+  <div class="main">
+    <h1>Record Request Form</h1>
+    <Form onSubmit={showResults}>
+    {({handleSubmit, values, submitting }) => (
+    <form onSubmit={handleSubmit}>
+    <Field 
+      name="firstName"
+        placeholder="First Name"
+        validate={required}
+        >{({input, meta, placeholder}) => (
+         <div>
+           <label>First Name: </label>
+           <input {...input} placeholder={placeholder} />
+           {meta.error && meta.touched && <span>{meta.error}</span>}
+         </div>
+    )}
+    </Field>
+    
+    <Field 
+      name="lastName"
+        placeholder="Last Name"
+        validate={required}
+        >{({input, meta, placeholder}) => (
+         <div>
+           <label>Last Name: </label>
+           <input {...input} placeholder={placeholder} />
+           {meta.error && meta.touched && <span>{meta.error}</span>}
+         </div>
+    )}
+    </Field>
+
+    <Field 
+      name="email"
+        placeholder="Email"
+        validate={required}
+        >{({input, meta, placeholder}) => (
+         <div>
+           <label>Email: </label>
+           <input {...input} placeholder={placeholder} />
+           {meta.error && meta.touched && <span>{meta.error}</span>}
+         </div>
+    )}
+    </Field>
+
+    <Field 
+      name="schoolDistrict"
+        placeholder="School District"
+        validate={required}
+        >{({input, meta, placeholder}) => (
+         <div>
+           <label>School District: </label>
+           <input {...input} placeholder={placeholder} />
+           {meta.error && meta.touched && <span>{meta.error}</span>}
+         </div>
+    )}
+    </Field>
+
+    <Field 
+      name="Record Series"
+        placeholder="Record Series"
+        validate={required}
+        >{({input, meta, placeholder}) => (
+         <div>
+           <label>Record Series: </label>
+           <input {...input} placeholder="SR, SPED, HR or AR" />
+           {meta.error && meta.touched && <span>{meta.error}</span>}
+         </div>
+    )}
+    </Field>
+
+    <button class="bttn" type="submit" disabled={submitting}>Submit</button>
+    <pre>{JSON.stringify(values, undefined, 2)}</pre>
+    </form>     
+    )}
+    </Form>
+  </div>
+)
+
+export default App
